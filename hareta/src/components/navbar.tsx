a58b6c1c39d0e51d19d55e6c0cec565e2732575c
NDERI007 <tnderi373@gmail.com>
2025-09-18 22:11:26 +0300

    drafting known locations

--- hareta/src/components/navbar.tsx
+++ hareta/src/components/navbar.tsx
@@ -1,19 +1,21 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <nav className="fixed top-0 left-0 bg-[#faf7ef] w-full z-30 border-b border-green-700">
       <div className="mx-auto flex max-w-6xl items-center justify-between px-2 py-4">
         {/* App name / logo */}
-        <a href="/" className="text-2xl font-bold text-green-900">
+        <Link to={"/"} className="text-2xl font-bold text-green-900">
           Foodie
-        </a>
+        </Link>
 
         {/* Login button */}
-        <a
-          href="/login"
+        <Link
+          to={"login"}
           className="rounded-lg font-medium text-xl text-green-900 hover:bg-amber-100 transition"
         >
           Login
-        </a>
+        </Link>
       </div>
     </nav>
   );
