4ddd44f0c17a28bc4465c6e6e4021ae505b844ce
NDERI007 <tnderi373@gmail.com>
2025-10-23 13:37:44 +0300

    forgot to include img

--- hareta/vite.config.ts
+++ hareta/vite.config.ts
@@ -3,20 +3,9 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-      '@admin': path.resolve(__dirname, './src/admin'),
-    },
-  },
   server: {
     proxy: {
       '/api': {
