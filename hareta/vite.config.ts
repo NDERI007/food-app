7209a529be0ec6502582595b3b97120992d82690
NDERI007 <tnderi373@gmail.com>
2025-10-23 13:32:47 +0300

    aliases

--- hareta/vite.config.ts
+++ hareta/vite.config.ts
@@ -3,9 +3,20 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
-// https://vite.dev/config/
+import path from 'path';
+
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@admin': path.resolve(__dirname, './src/admin'),
+    },
+  },
   server: {
     proxy: {
       '/api': {
