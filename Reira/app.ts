c8e6d62ccdf6361953deaa2390000f5de7b50ac1
NDERI007 <tnderi373@gmail.com>
2025-10-31 20:34:38 +0300

    FIX

--- Reira/app.ts
+++ Reira/app.ts
@@ -35,7 +35,7 @@ app.use(cookieParser());
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
